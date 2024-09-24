8073372023703648bac696338b8c61f534caeeba
Igor Klemenski <igor.klemenski@microsoft.com>
2024-09-24 22:37:05 +0000

    feat: add dynamically configured Hubble metrics
    Signed-off-by: Igor Klemenski <igor.klemenski@microsoft.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -1031,6 +1031,9 @@ const (
 	// HubbleMetrics specifies enabled metrics and their configuration options.
 	HubbleMetrics = "hubble-metrics"
 
+	// HubbleDynamicMetricConfigFilePath specifies the filepath with configuration of hubble metrics.
+	HubbleDynamicMetricConfigFilePath = "hubble-dynamic-metrics-config-path"
+
 	// HubbleFlowlogsConfigFilePath specifies the filepath with configuration of hubble flowlogs.
 	// e.g. "/etc/cilium/flowlog.yaml"
 	HubbleFlowlogsConfigFilePath = "hubble-flowlogs-config-path"
@@ -2215,6 +2218,9 @@ type DaemonConfig struct {
 
 	// HubbleMetrics specifies enabled metrics and their configuration options.
 	HubbleMetrics []string
+
+	// HubbleDynamicMetricConfigFilePath specifies the filepath with configuration of hubble metrics.
+	HubbleDynamicMetricConfigFilePath string
 
 	// HubbleFlowlogsConfigFilePath specifies the filepath with configuration of hubble flowlogs.
 	// e.g. "/etc/cilium/flowlog.yaml"
@@ -3514,6 +3520,8 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.HubbleMetricsServerTLSKeyFile = vp.GetString(HubbleMetricsTLSKeyFile)
 	c.HubbleMetricsServerTLSClientCAFiles = vp.GetStringSlice(HubbleMetricsTLSClientCAFiles)
 	c.HubbleMetrics = vp.GetStringSlice(HubbleMetrics)
+
+	c.HubbleDynamicMetricConfigFilePath = vp.GetString(HubbleDynamicMetricConfigFilePath)
 
 	c.HubbleExportFilePath = vp.GetString(HubbleExportFilePath)
 	c.HubbleExportFileMaxSizeMB = vp.GetInt(HubbleExportFileMaxSizeMB)
